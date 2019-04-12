3f41a3fcc9008db1590ef50a1fa0f41d7a5257bd
Vitor Daniel <vitor.daniel@outlook.pt>
2019-04-12 11:27:06 +0100

    EX01 - method + variables

---
+++ BookManager/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookManager
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        public Book(string title , string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+         public string GetTitle()
+        {
+            if title < 0
+                return title;
+        }
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        
+        }
+        public void AssignWordCountFromText(string text)
+        {
+            wordCount = text.Split(' ').Length;
+        }
+    }
+}
